bb2e6e3193dbcc7a6e9d84f3621231098dc709ce
Your Name <you@example.com>
2018-11-10 10:33:37 +0100

    first commit

---
+++ resources/store.py
@@ -0,0 +1,32 @@
+from flask_restful import Resource
+from models.store import StoreModel
+
+class Store(Resource):
+    def get(self, name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            return store.json()
+        return {'message': '{} not found'.format(name)}, 404
+
+    def post(self, name):
+        if StoreModel.find_by_name(name):
+            return {'message': "The store {} already exists!".format(name)}, 400
+        
+        store = StoreModel(name)
+        try:
+            store.save_to_db()
+        except:
+            return {'message': "An error ocurred while creating store {}".format(name)}, 500
+
+        return store.json()
+
+    def delete(self, name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            store.delete_from_db()
+
+        return {'message:' 'Store deleted'}
+
+class StoreList(Resource):
+    def get(self):
+        return {'stores': [store.json() for store in StoreModel.query.all()]}
